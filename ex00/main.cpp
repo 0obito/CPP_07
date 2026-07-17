e7832335b3ea2ca674d369c750af1bd646e2fcc3
0obito <elmsafer.abdellah00@gmail.com>
2026-07-17 16:36:18 +0100

    EX00 SHould be done I believe.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -14,6 +14,7 @@ class A {
                 _a = other._a;
             return *this;
         }
+        ~A(){};
         int getVal() {return _a;}
         bool operator<(const A& other) {
             if (_a < other._a) {
@@ -33,41 +34,80 @@ class A {
         }
 };
 
+class B {
+    private:
+        int _b;
+    public:
+        B():_b(0){}
+        B(int value):_b(value){}
+        B(const B& other): _b(other._b){}
+        B& operator=(const B& other){
+            if (this != &other)
+                _b = other._b;
+            return *this;
+        }
+        ~B(){}
+        int getValue(){return _b;}
+};
+
 int main() {
+    int x = 9, y = 10;
     A aA(99), bA(66);
-    int a = 9, b = 10;
 
-    std::cout<<"---------- 1: SWAP tests: ----------\n";
+    std::cout<<"---------- 1: SWAP tests: ----------\n\n";
 
-    std::cout<<"a = "<<a<<" and b = "<<b<<"\n";
-    swap(a, b);
+    std::cout<<"x = "<<x<<" and y = "<<y<<"\n";
+    swap(x, y);
     std::cout<<"swap performed\n";
-    std::cout<<"a = "<<a<<" and b = "<<b<<"\n";
+    std::cout<<"x = "<<x<<" and y = "<<y<<"\n\n";
 
     std::cout<<"aA = "<<aA.getVal()<<" and bA = "<<bA.getVal()<<"\n";
     swap(aA, bA);
     std::cout<<"swap performed\n";
-    std::cout<<"aA = "<<aA.getVal()<<" and bA = "<<bA.getVal()<<"\n";
+    std::cout<<"aA = "<<aA.getVal()<<" and bA = "<<bA.getVal()<<"\n\n";
 
 
-    std::cout<<"---------- 2: MIN tests: ----------\n";
+    std::cout<<"---------- 2: MIN tests: ----------\n\n";
     // back to how it was first
-    swap(a, b);
+    swap(x, y);
     swap(aA, bA);
 
-    std::cout<<"a = "<<a<<" and b = "<<b<<"\n";
-    std::cout<<"The min value between a and b is: "<<min(a, b)<<"\n";
+    std::cout<<"x = "<<x<<" and y = "<<y<<"\n";
+    std::cout<<"The min value between x and y is: "<<min(x, y)<<"\n\n";
 
     std::cout<<"aA = "<<aA.getVal()<<" and bA = "<<bA.getVal()<<"\n";
-    std::cout<<"The min value between aA and bA is: "<<min(aA, bA).getVal()<<"\n";
+    std::cout<<"The min value between aA and bA is: "<<min(aA, bA).getVal()<<"\n\n";
 
-    std::cout<<"---------- 3: MAX tests: ----------\n";
+    std::cout<<"---------- 3: MAX tests: ----------\n\n";
 
-    std::cout<<"a = "<<a<<" and b = "<<b<<"\n";
-    std::cout<<"The max value between a and b is: "<<max(a, b)<<"\n";
+    std::cout<<"x = "<<x<<" and y = "<<y<<"\n";
+    std::cout<<"The max value between x and y is: "<<max(x, y)<<"\n\n";
 
     std::cout<<"aA = "<<aA.getVal()<<" and bA = "<<bA.getVal()<<"\n";
-    std::cout<<"The max value between aA and bA is: "<<max(aA, bA).getVal()<<"\n";
+    std::cout<<"The max value between aA and bA is: "<<max(aA, bA).getVal()<<"\n\n";
+
+    std::cout<<"---------- end of my personal tests ----------\n";
+
+
+    std::cout<<"\n\n---------- subject suggested tests ----------\n\n";
+
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+    std::cout<<"\n---------- end of subject tests ----------\n";
 
     return 0;
 }
+
+
+
